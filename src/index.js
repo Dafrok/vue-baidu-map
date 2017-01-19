2cfcbf1d3d866e75441cfd411b6d2125b4a7ccbb
Dafrok <o.o@mug.dog>
2017-01-19 19:49:23 +0800

    Global install

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import BaiduMap from './Map.vue'
-import ScaleControl from './controls/ScaleControl.vue'
-import NavigationControl from './controls/NavigationControl.vue'
-import MapTypeControl from './controls/MapTypeControl.vue'
-import OverviewMapControl from './controls/OverviewMapControl.vue'
+import Map from './map/Map.vue'
+import MapView from './map/MapView.vue'
+import ScaleControl from './controls/Scale.vue'
+import NavigationControl from './controls/Navigation.vue'
+import MapTypeControl from './controls/MapType.vue'
+import OverviewMapControl from './controls/OverviewMap.vue'
 
-export default BaiduMap
-export {ScaleControl, NavigationControl, MapTypeControl, OverviewMapControl}
+export default {
+  install (Vue, options) {
+    const {ak} = options
+    Vue.prototype._BMap = () => {
+      return {
+        ak
+      }
+    }
+    Vue.component('baidu-map', Map)
+    Vue.component('map-view', MapView)
+    Vue.component('map-control-scale', ScaleControl)
+    Vue.component('map-control-navigation', NavigationControl)
+    Vue.component('map-control-map-type', MapTypeControl)
+    Vue.component('map-control-overview-map', OverviewMapControl)
+  }
+}
