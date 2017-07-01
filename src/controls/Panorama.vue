a57842fa8e17d8e557692db182e3921bf57bbb85
Dafrok <o.o@mug.dog>
2017-07-01 23:41:13 +0800

    Passing build

--- src/controls/Panorama.vue
+++ src/controls/Panorama.vue
@@ -18,7 +18,7 @@ export default {
     load () {
       const {BMap, map, anchor, offset} = this
       this.originInstance = new BMap.PanoramaControl({
-        anchor: global[this.anchor],
+        anchor: global[anchor],
         offset
       })
       map.addControl(this.originInstance)
