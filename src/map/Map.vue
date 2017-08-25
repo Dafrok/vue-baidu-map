d2553c2786b4ca8abc0e773c3f8913b9b62ee0ec
马金花儿 <o.o@mug.dog>
2017-08-25 11:21:59 +0800

    #125

--- src/map/Map.vue
+++ src/map/Map.vue
@@ -221,7 +221,7 @@ export default {
           }
           const $script = document.createElement('script')
           global.document.body.appendChild($script)
-          $script.src = `//api.map.baidu.com/api?v=2.0&ak=${ak}&callback=_initBaiduMap`
+          $script.src = `https://api.map.baidu.com/api?v=2.0&ak=${ak}&callback=_initBaiduMap`
         })
         return global.BMap._preloader
       } else if (!global.BMap._preloader) {
