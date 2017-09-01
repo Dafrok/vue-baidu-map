5c85b13b808ff5543a60bfec915c9b76869cc4f1
Dafrok <o.o@mug.dog>
2017-09-01 09:33:55 +0800

    change docs template & style type

--- docs/components/DocPreview.vue
+++ docs/components/DocPreview.vue
@@ -1,27 +1,22 @@
-<template>
-<div class="demo-card-wide mdl-card mdl-shadow--4dp">
-  <slot></slot>
-  <slot name="bottom"></slot>
-</div>
+<template lang="pug">
+div.demo-card-wide.mdl-card.mdl-shadow--4dp
+  slot
+  slot(name="bottom")
 </template>
 
 <style lang="stylus" scoped>
-.mdl-card {
-  overflow: hidden;
-  width: 100%;
-}
-.mdl-card__title {
-  background-color: #f2f2f2;
-}
-.mdl-spinner {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-.mdl-textfield {
-  width: 100%;
-}
+.mdl-card 
+  overflow hidden
+  width 100%
+.mdl-card__title 
+  background-color #f2f2f2
+.mdl-spinner 
+  position absolute
+  top 0
+  bottom 0
+  left 0
+  right 0
+  margin auto
+.mdl-textfield 
+  width 100%
 </style>
