709034c5f0b8b2199f6117387c7c3ad010ef7e00
Dafrok <o.o@mug.dog>
2018-01-03 13:29:13 +0800

    fixed #213

--- src/base/bindEvent.js
+++ src/base/bindEvent.js
@@ -1,6 +1,8 @@
 import events from './events.js'
+import {toKebabCase} from './util.js'
+
 export default function (instance, eventList) {
-  (eventList || events[this.$options._componentTag]).forEach(event => {
+  (eventList || events[toKebabCase(this.$options._componentTag)]).forEach(event => {
     const hasOn = event.slice(0, 2) === 'on'
     instance.addEventListener(event, (arg) => {
       this.$emit(hasOn ? event.slice(2) : event, arg)
