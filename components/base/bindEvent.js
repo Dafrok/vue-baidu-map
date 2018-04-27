6f97617a0216c923fca0b611bd54836329abd17e
马金花儿 <o.o@mug.dog>
2018-04-27 18:12:59 +0800

    fix #353

--- components/base/bindEvent.js
+++ components/base/bindEvent.js
@@ -5,8 +5,8 @@ export default function (instance, eventList) {
   const ev = eventList || events[toKebabCase(this.$options._componentTag)]
   ev && ev.forEach(event => {
     const hasOn = event.slice(0, 2) === 'on'
-    instance.addEventListener(event, (arg) => {
-      this.$emit(hasOn ? event.slice(2) : event, arg)
-    })
+    const eventName = hasOn ? event.slice(2) : event
+    const listener = this.$listeners[eventName]
+    listener && instance.addEventListener(event, listener.fns)
   })
 }
